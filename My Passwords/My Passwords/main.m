f70a18c32cc226621c11630d67aae13eb3dc9d5c
EversonTrindade <eversonpereira@ymail.com>
2015-12-08 22:58:58 -0200

    First Commit

---
+++ My Passwords/My Passwords/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  My Passwords
+//
+//  Created by Everson Trindade on 12/8/15.
+//  Copyright © 2015 SP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ My Passwords/My PasswordsTests/My_PasswordsTests.m
@@ -0,0 +1,39 @@
+//
+//  My_PasswordsTests.m
+//  My PasswordsTests
+//
+//  Created by Everson Trindade on 12/8/15.
+//  Copyright © 2015 SP. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface My_PasswordsTests : XCTestCase
+
+@end
+
+@implementation My_PasswordsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
